7382d1217b2c5dca71a6b4a5daa8e9ba6ac5bbed
Maverick130 <brucef787@gmail.com>
2023-06-18 15:38:59 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - main entry
+  * Return: 0 success
+  */
+int main(void)
+{
+/* introducing a variable */
+	char bruce;
+
+	for (bruce = 'z'; bruce >= 'a'; bruce--)
+	{
+		putchar('n');
+	}
+	putchar('\n');
+	return (0);
+}
