c6d00b6151187bacfd42fa00e19051b1dd3732d2
Maverick130 <brucef787@gmail.com>
2023-06-18 09:35:03 -0700

    what i call alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,22 +9,21 @@ int main(void)
 {
 /* declarin variable*/
 	int n;
+	int last_digit = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_num = n % 10;
-
-	if (last_num > 5)
+	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, last_num);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	else if (last_num == 0)
+	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, last_num);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than6 and not 0", n, last_num);
+		printf("Last digit of %d is %d and is less than6 and not 0\n", n, last_digit);
 	}
 
 	return (0);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: 0 success
+  */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('n');
+		return (0);
+}
