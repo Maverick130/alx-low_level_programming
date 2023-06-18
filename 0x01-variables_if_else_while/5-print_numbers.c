1d91fe5d1b566d2d36dd8787108df3c13716aebe
Maverick130 <brucef787@gmail.com>
2023-06-18 11:12:07 -0700

    printing numbers from 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - main entry
+  * Return: 0 success
+  *
+  */
+int main(void)
+{
+/* declaring a variable*/
+	int b;
+
+/* calling the for loop*/
+	for (b = 0; b < 10; b++)
+	{
+		printf("%d\n", b);
+	}
+	return (0);
+}
