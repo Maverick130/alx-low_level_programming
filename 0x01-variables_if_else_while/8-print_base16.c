2f5de720cb8b3086e727c9b2e41260fbc989fc05
Maverick130 <brucef787@gmail.com>
2023-06-19 19:06:50 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,27 @@
-#:
+#include <stdio.h>
+/**
+   * main - main entry
+   * description: print all number of base 16
+   * Return: 0 success
+   */
+int main(void)
+{
+/* declaring varieable */
+	int g;
+
+/* calling the for loop */
+	for (g = 0; g < 16; g++)
+	{
+/* introducing if function */
+		if (g < 10)
+		{
+			putchar(g + '0');
+		}
+		else
+		{
+			putchar(g - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
