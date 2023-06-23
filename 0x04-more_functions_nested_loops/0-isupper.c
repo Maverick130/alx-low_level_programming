2a29382f9998cc0d2c51dbdd0b23f003b218adfd
Maverick130 <brucef787@gmail.com>
2023-06-23 11:57:19 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - prints uppercase letter
+ *
+ * @c: it checks the code
+ *
+ * Return: 0 success
+ */
+int _isupper(int c)
+{
+	if (c >= 65, &&c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
