d54cad25663563d18a45ec09ce10f96fcb4a697f
Maverick130 <brucef787@gmail.com>
2023-06-23 15:35:28 -0700

    i believe in number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return the value of a*b
+ */
+int mul(int a, int b)
+{
+	int answer = a * b;
+
+	return (answer);
+}
