27e22012b1f6865d2134367561bdf8c71bde8103
Maverick130 <brucef787@gmail.com>
2023-06-26 15:26:28 -0700

    battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - takes a pointer to an integer
+ *
+ * Return: always 0
+ * @n: declear variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
