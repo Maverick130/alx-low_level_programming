1cac41382e790d26504739dc0940381b513bc83f
Maverick130 <brucef787@gmail.com>
2023-06-27 23:33:30 -0700

    coppy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * *
+ * *_strcpy - copies the strings pointer
+ * @dest: first variable
+ * @src: second variable
+ * Return: 0 succes
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (temp);
+}
