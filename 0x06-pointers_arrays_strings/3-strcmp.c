0c831e8dcaffc2277bb7a9b287911c01369bc38a
Maverick130 <brucef787@gmail.com>
2023-06-29 13:53:02 -0700

    strcompare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first variable
+ * @s2: second variable
+ *
+ * Return: no return value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int _strcmp(char *s1, char *s2);
+}
